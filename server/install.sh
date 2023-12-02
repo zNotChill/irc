f598ac9081ded71416126efa34699ccad76a742b
zNotChill <znotchill@znci.dev>
2023-12-02 23:46:57 +0000

    Fix install script

--- server/install.sh
+++ server/install.sh
@@ -7,7 +7,9 @@ git clone https://github.com/zNotChill/irc.git irc
 apt install npm -y
 npm install pm2 -g 
 
-cd irc/server
+cd irc
+git pull
+cd server
 npm install
 
 pm2 start server.js --name irc
