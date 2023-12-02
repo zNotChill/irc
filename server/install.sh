9fcc9bb01c9d6c7b123bcc03715fde47191a5396
zNotChill <znotchill@znci.dev>
2023-12-02 22:04:59 +0000

    HI

--- server/install.sh
+++ server/install.sh
@@ -1,10 +1,11 @@
+#! /bin/bash
 
 # Installer for the server
 
 git clone https://github.com/zNotChill/irc.git irc
 
-apt install npm
-npm install pm2 -g
+yes | apt install npm
+npm install pm2 -g 
 
 cd irc/server
 npm install
